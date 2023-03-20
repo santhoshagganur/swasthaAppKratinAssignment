f78ee05cfae88ea63f37c4136f9b88e56d52668a
santhoshagganur <118893483+santhoshagganur@users.noreply.github.com>
2023-03-20 13:05:57 +0530

    updated new

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import {BrowserRouter, Route, Switch} from 'react-router-dom'
 
 import Home from './components/Home'
+import Above65 from './components/Above65'
 import NotFound from './components/NotFound'
 
 import './App.css'
@@ -9,6 +10,7 @@ const App = () => (
   <BrowserRouter>
     <Switch>
       <Route exact path="/" component={Home} />
+      <Route exact path="/above65" component={Above65} />
       <Route component={NotFound} />
     </Switch>
   </BrowserRouter>
