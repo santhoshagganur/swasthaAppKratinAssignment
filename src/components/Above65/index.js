0b98bd7df4f87114396115ba0755fb0d5097abc8
santhoshagganur <118893483+santhoshagganur@users.noreply.github.com>
2023-03-20 13:01:14 +0530

    completed new feature

--- src/components/Above65/index.js
+++ src/components/Above65/index.js
@@ -1,1 +1,118 @@
+import {Component} from 'react'
+import './index.css'
+
+class Above65 extends Component {
+  render() {
+    return (
+      <div className="keys-help-container">
+        <h1 className="suggestion-main-heading">
+          The major factors can change your life
+        </h1>
+        <ul className="suggestions-container">
+          <li className="keys-to-happy-container">
+            <h1 className="suggestion-heading"> Food And Diet Plan </h1>
+            <p className="suggestion-description">
+              The dietary requirements of senior citizens are different from
+              others. Hence, they must maintain a diet plan that aligns with
+              their health needs. Here, they must note that body mass and
+              metabolism slows down with advancing age, bringing changes in the
+              digestive system, food preferences, and appetite.
+            </p>
+            <ul className="some-requires">
+              <li> Fruits and vegetables </li>
+              <li> Low-fat dairy items </li>
+              <li> Low-sodium canned vegetables </li>
+              <li> Nuts and seeds </li>
+            </ul>
+          </li>
+
+          <li className="keys-to-happy-container">
+            <h1 className="suggestion-heading"> Daily Walk </h1>
+            <p className="suggestion-description">
+              Just 30 minutes every day can increase cardiovascular fitness,
+              strengthen bones, reduce excess body fat, and boost muscle power
+              and endurance. It can also reduce your risk of developing
+              conditions such as heart disease, type 2 diabetes, osteoporosis
+              and some cancers.
+            </p>
+            <ul className="some-requires">
+              <li> Improve Heart Health </li>
+              <li> Lower Blood Sugar </li>
+              <li> Improve Mental Health </li>
+              <li> Social Engagement </li>
+            </ul>
+          </li>
+
+          <li className="keys-to-happy-container">
+            <h1 className="suggestion-heading"> Spending Time With Family </h1>
+            <p className="suggestion-description">
+              The holidays are the perfect time to gather with loved ones--
+              sharing stories that ignite laughter and good cheer. Engaging in
+              these precious moments with family and friends is not only
+              enjoyable, it’s also beneficial to your health.
+            </p>
+            <ul className="some-requires">
+              <li> Helps you cope with stress </li>
+              <li> May lengthen your life </li>
+              <li> Improve Mental Health </li>
+              <li> Enhances Physical Health </li>
+            </ul>
+          </li>
+
+          <li className="keys-to-happy-container">
+            <h1 className="suggestion-heading"> Regular Health Checkups </h1>
+            <p className="suggestion-description">
+              A regular check-up can help in recovering your body from any
+              health concerns. Increases life span – by doing a regular check-up
+              of your body and health, you can be assured of how healthy you
+              are. By taking the preventive advice from the doctor and following
+              the doctor prescriptions can increase your lifespan as well.
+            </p>
+            <ul className="some-requires">
+              <li> Prevent health issues </li>
+              <li> Lower health care costs </li>
+              <li> Eliminates further growth of disease </li>
+              <li> Reduces the risk of complication during treatment </li>
+            </ul>
+          </li>
+
+          <li className="keys-to-happy-container">
+            <h1 className="suggestion-heading"> Yoga </h1>
+            <p className="suggestion-description">
+              Retirement is the perfect time to try some new things and pick up
+              some healthier habits you may not have had time to consider in
+              your working years. If you’ve never tried yoga before, just follow
+              today.
+            </p>
+            <ul className="some-requires">
+              <li> Improved sleeping habits </li>
+              <li> Alleviate aches and pains </li>
+              <li> Reduced stress </li>
+              <li> Strengthened bones </li>
+            </ul>
+          </li>
+
+          <li className="keys-to-happy-container">
+            <h1 className="suggestion-heading">
+              Recall your favorite past moments in your life
+            </h1>
+            <p className="suggestion-description">
+              Taking a walk down “memory lane” is one of the best things seniors
+              can do for their mind and body. When we feel that it moves us to
+              the past memories which are once recreated in front of us.
+            </p>
+            <ul className="some-requires">
+              <li> Reduction in negative emotions </li>
+              <li> Promotes Mental Health </li>
+              <li> Reduced stress </li>
+              <li> Eliminates Boredom </li>
+            </ul>
+          </li>
+        </ul>
+      </div>
+    )
+  }
+}
+
+export default Above65
 
