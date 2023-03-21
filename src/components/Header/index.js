d550b7be19dff7164e99831dc86a885f3f66823f
santhoshagganur <118893483+santhoshagganur@users.noreply.github.com>
2023-03-21 14:04:59 +0530

    adding more features

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -3,13 +3,20 @@ import './index.css'
 
 const Header = () => (
   <nav className="navbar-container">
-    <h1 className="app-name"> Swastha </h1>
+    <img
+      src="https://res.cloudinary.com/dkrgkqehf/image/upload/v1679329928/Free_Sample_By_Wix_gasnec.jpg"
+      alt="swastha"
+      className="logo"
+    />
     <ul className="app-features-container">
       <Link to="/">
         <li className="nav-item"> Home </li>
       </Link>
       <Link to="/above65">
         <li className="nav-item"> Above65 </li>
+      </Link>
+      <Link to="/OnlineLabs">
+        <li className="nav-item"> Online Labs </li>
       </Link>
     </ul>
   </nav>
