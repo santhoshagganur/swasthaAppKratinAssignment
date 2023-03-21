e8fa3ab7876d1f4c639bed76f88eab443f860837
santhoshagganur <118893483+santhoshagganur@users.noreply.github.com>
2023-03-21 14:16:33 +0530

    added feature

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import {BrowserRouter, Route, Switch} from 'react-router-dom'
 
 import Home from './components/Home'
 import Above65 from './components/Above65'
+import OnlineLabs from './components/OnlineLabs'
 import NotFound from './components/NotFound'
 
 import './App.css'
@@ -11,6 +12,7 @@ const App = () => (
     <Switch>
       <Route exact path="/" component={Home} />
       <Route exact path="/above65" component={Above65} />
+      <Route exact path="/OnlineLabs" component={OnlineLabs} />
       <Route component={NotFound} />
     </Switch>
   </BrowserRouter>
